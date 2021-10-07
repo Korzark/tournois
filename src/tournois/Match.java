5357aff0b4114a26f106ee99492cdd21986fa080
Frédéric Fondement <frederic.fondement@uha.fr>
2021-10-07 17:53:02 +0200

    performing first match

--- src/tournois/Match.java
+++ src/tournois/Match.java
@@ -5,6 +5,7 @@ import java.util.Set;
 public class Match {
 
 	private Set<Team> teams;
+	private Team winner;
 
 	public Match(Set<Team> teams) {
 		this.teams = teams;
@@ -12,6 +13,21 @@ public class Match {
 
 	public Set<Team> getTeams() {
 		return this.teams;
+	}
+
+	public void play(Team t1, int scoreT1, Team t2, int scoreT2) {
+		if (scoreT2 > scoreT1) {
+			this.winner = t2;
+		} else if (scoreT1 > scoreT2) {
+			this.winner = t1;
+		} else {
+			this.winner = null;
+		}
+	}
+
+	public Team getWinner() {
+		// TODO Auto-generated method stub
+		return this.winner;
 	}
 
 }
--- test/tournois/TestTournois.java
+++ test/tournois/TestTournois.java
@@ -32,5 +32,70 @@ class TestTournois {
 		Match m = list.iterator().next();
 		assertEquals(Set.of(t1, t2),m.getTeams());
 	}
+	
+	@Test
+	void twoTeamsMatchT2Wins() {
+		
+		Tournois sut = new Tournois(); //System under test
+		
+		Team t1 = new Team("teamOne");
+		Team t2 = new Team("teamTwo");
+		
+		sut.add(t1);
+		sut.add(t2);
+		
+		sut.launch();
+		Bracket res = sut.getBracket();
+		Collection<Match> list = res.getNextMatches();
+		
+		Match m = list.iterator().next();
+		m.play(t1, 80, t2, 90);
+		Team winner = m.getWinner();
+		assertEquals(t2, winner);
+	}
+	
+	@Test
+	void twoTeamsMatchT1Wins() {
+		
+		Tournois sut = new Tournois(); //System under test
+		
+		Team t1 = new Team("teamOne");
+		Team t2 = new Team("teamTwo");
+		
+		sut.add(t1);
+		sut.add(t2);
+		
+		sut.launch();
+		Bracket res = sut.getBracket();
+		Collection<Match> list = res.getNextMatches();
+		
+		Match m = list.iterator().next();
+		m.play(t1, 110, t2, 90);
+		Team winner = m.getWinner();
+		assertEquals(t1, winner);
+	}
+	
+	@Test
+	void twoTeamsMatchEquality() {
+		
+		Tournois sut = new Tournois(); //System under test
+		
+		Team t1 = new Team("teamOne");
+		Team t2 = new Team("teamTwo");
+		
+		sut.add(t1);
+		sut.add(t2);
+		
+		sut.launch();
+		Bracket res = sut.getBracket();
+		Collection<Match> list = res.getNextMatches();
+		
+		Match m = list.iterator().next();
+		m.play(t1, 90, t2, 90);
+		Team winner = m.getWinner();
+		assertNull(winner);
+	}
+	
+	
 
 }
